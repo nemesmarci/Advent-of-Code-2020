31d5fcba4bcc7f7f95cf95b3af77b3fb4eaef01f
Marcell Nemes <nemes@sch.bme.hu>
2020-12-10 12:19:32 +0100

    Refactor: use math.prod instead of reduce(mul, ...)

--- 1/common.py
+++ 1/common.py
@@ -1,10 +1,9 @@
 from itertools import combinations
-from functools import reduce
-from operator import mul
+from math import prod
 
 
 def solve(n):
     with open('input.txt') as data:
-        return (next(reduce(mul, combination)
+        return (next(prod(combination)
                      for combination in combinations(map(int, data), n)
                      if sum(combination) == 2020))
